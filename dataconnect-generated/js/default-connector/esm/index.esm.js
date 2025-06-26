2b293cb342ab6bd788fa4d2d9fa73eefe21297c8
caregivespot <126506945+caregivespot@users.noreply.github.com>
2025-06-26 01:43:54 -0700

    Skip local Windows build - let Cloud Run build in Linux environment

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'afhoffice',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'afhoffice',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
